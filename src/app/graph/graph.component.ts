5fb7eb650f698b0dc87b3a9aa0be535b7a01b3a5
razvantomegea <razvan.tomegea@gmail.com>
2018-03-11 13:59:30 +0200

    implemented zooming

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -13,10 +13,10 @@ const LINKS: GraphLink[] = [
 ];
 
 const NODES: GraphNode[] = [
-  new GraphNode(0, 225, 100, 15, '#FFB300', 'Bob'),
+  new GraphNode(0, 225, 100, 30, '#FFB300', 'Bob'),
   new GraphNode(1, 400, 300, 30, '#FFB300', 'Jerry'),
-  new GraphNode(2, 500, 500, 20, '#ffff00', 'John'),
-  new GraphNode(3, 800, 100, 50, '#00ff00', 'George')
+  new GraphNode(2, 500, 500, 30, '#ffff00', 'John'),
+  new GraphNode(3, 800, 100, 30, '#00ff00', 'George')
 ];
 
 @Component({
@@ -29,17 +29,35 @@ export class GraphComponent implements OnInit {
   private node: d3.Selection<d3.BaseType, d3.Group, d3.BaseType, any>;
   private simulation: d3.Simulation<GraphNode, GraphLink>;
   private svg: d3.Selection<d3.BaseType, {}, HTMLElement, any>;
-
   constructor(private graphSvc: GraphService) { }
 
   ngOnInit(): void {
+
     this.svg = d3.select('.flowChart')
       .append('svg')
       .attr('width', '100%')
-      .attr('height', '100%');
+      .attr('height', '100%')
+      .append('g')
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .call(d3.zoom()
+        .on('zoom', () => {
+          this.svg.attr('transform', `translate(${d3.event.transform.x}, ${d3.event.transform.y}) scale(${d3.event.transform.k})`);
+        })
+      );
+
+    const rect = this.svg
+      .append('rect')
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .style('fill', 'none')
+      .style('pointer-events', 'all');
+
+    const container = this.svg.append('g');
+
     this.setupForceLayout();
-    this.setupNodes();
     this.setupLinks();
+    this.setupNodes();
 
     this.simulation.on('tick', () => {
       this.link.attr('x1', (d: GraphLink) => (<GraphNode>d.source).x)
@@ -53,27 +71,21 @@ export class GraphComponent implements OnInit {
 
   private setupForceLayout(): void {
     this.simulation = d3.forceSimulation(NODES).alphaDecay(0.01)
-      .force('charge', d3.forceManyBody()
-      // .strength(80).distanceMax(400).distanceMin(80)
-      )
+      .velocityDecay(0.55)
       .force('link', d3.forceLink(LINKS)
         .distance((l: GraphLink) => l.distance)
         .strength((l: GraphLink) => l.strength)
       )
-    // .force('center', d3.forceCenter(container.attr('width') / 2, container.attr('height') / 2))
-    // .force('y', d3.forceY(0.001))
-    // .force('x', d3.forceX(0.001))
-    // .force('attractForce', d3.forceManyBody().strength(80).distanceMax(400).distanceMin(80))
-    // .force('collisionForce', d3.forceCollide(12).strength(1).iterations(100));
+      .force('charge', d3.forceManyBody().strength(-100).distanceMin(10000))
+      .force('collide', d3.forceCollide(45).strength(1));
   }
 
   private setupLinks(): void {
-    this.link = this.svg.selectAll('.link')
+    this.link = this.svg.selectAll('line')
       .data(LINKS)
       .enter().append('line')
-      .attr('class', 'link')
-      .attr('stroke', '#fff')
-      .attr('stroke-width', '2px')
+      .attr('stroke', 'rgba(255, 255, 255, .5)')
+      .attr('stroke-width', '1px');
   }
 
   private setupNodes(): void {
@@ -83,8 +95,9 @@ export class GraphComponent implements OnInit {
       .attr('class', 'node')
       .call(d3.drag()
         .on('start', (d: GraphNode) => {
-          this.simulation.restart();
-          this.simulation.alpha(0.7);
+          if (!d3.event.active) {
+            this.simulation.alphaTarget(0.3).restart();
+          }
           d.fx = d.x;
           d.fy = d.y;
         })
@@ -93,14 +106,14 @@ export class GraphComponent implements OnInit {
           d.fy = d3.event.y;
         })
         .on('end', (d: GraphNode) => {
-          d.fx = null;
-          d.fy = null;
-          this.simulation.alphaTarget(0.1);
+          if (!d3.event.active) {
+            this.simulation.alphaTarget(0);
+          }
+          delete d.fx;
+          delete d.fy;
         }));
 
     this.node.append('circle')
-      // .attr('cx', d => d.x)
-      // .attr('cy', d => d.y)
       .attr('r', (d: GraphNode) => d.r)
       .attr('fill', (d: GraphNode) => d3.color(d.color))
       .attr('stroke', '#fff')
@@ -109,7 +122,8 @@ export class GraphComponent implements OnInit {
     this.node.append('text')
       .attr('dy', '.33em')
       .attr('text-anchor', 'middle')
-      .style('font-family', 'Roboto')
+      .style('font', 'normal small-caps 300 16px "Roboto", sans-serif')
+      .style('color', 'silver')
       .text((d: GraphNode) => d.name);
   }
 }
